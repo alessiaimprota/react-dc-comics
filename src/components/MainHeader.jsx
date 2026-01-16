8e54862f532022310e0a3bc44b44b03910c84a04
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-16 17:22:23 +0100

    add new stuff

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,7 +2,25 @@ function MainHeader() {
 
   return (
     <>
-      <h1>Header</h1>
+      <div className="header">
+        <div class="logo">
+            <img src="../public/dc-logo.png" alt="logo-dc" />
+        </div>
+        <div className="navbar">
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+      </div>
     </>
   )
 }
